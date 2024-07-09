cd94e28154b581a68f137851a4321688770fd9a0
Muniier_ <108180866+munieer-bali@users.noreply.github.com>
2024-07-09 19:10:03 +0700

    upload fullstack crud express

---
+++ backend/controllers/UserController.js
@@ -0,0 +1,65 @@
+import User from "../models/UserModel.js";
+
+export const getUsers =  async(req, res)=> {
+    try {
+        const response =  await User.findAll();
+        res.status(200).json(response);
+    } catch (error) {
+        console.log(error.message);
+    }
+}
+
+//get user by id
+
+export const getUsersByid =  async(req, res)=> {
+    try {
+        const response =  await User.findOne({
+            where:{
+                id: req.params.id
+            }
+        });
+        res.status(200).json(response);
+    } catch (error) {
+        console.log(error.message);
+    }
+}
+
+//create user
+
+export const createUser =  async(req, res)=> {
+    try {
+       await User.create(req.body)
+        res.status(201).json({msg:"user created..."});
+    } catch (error) {
+        console.log(error.message);
+    }
+}
+
+//update user
+
+export const updateUser =  async(req, res)=> {
+    try {
+       await User.update(req.body, {
+         where: {
+            id: req.params.id
+         }
+       })
+        res.status(200).json({msg:"user updated..."});
+    } catch (error) {
+        console.log(error.message);
+    }
+}
+
+//delete user
+export const deleteUser =  async(req, res)=> {
+    try {
+       await User.destroy( {
+         where: {
+            id: req.params.id
+         }
+       })
+        res.status(200).json({msg:"user deleted..."});
+    } catch (error) {
+        console.log(error.message);
+    }
+}
